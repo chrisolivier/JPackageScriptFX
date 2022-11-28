4796e79ae897287d31b7c82f0b573705fd494dac
christophe <christophe.olivier@colhome.fr>
2022-11-28 17:21:58 +0100

    changing dependency to org.colhome.fxlauncher

--- jpackagefx-module1/src/main/java/com/dlsc/jpackage/module1/ModuleLabel1.java
+++ jpackagefx-module1/src/main/java/com/dlsc/jpackage/module1/ModuleLabel1.java
@@ -5,6 +5,6 @@ import javafx.scene.control.Label;
 public class ModuleLabel1 extends Label {
 
     public ModuleLabel1() {
-        super("Module Label 1-2-2");
+        super("Module Label 1-2-3");
     }
 }
